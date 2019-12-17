b0686d074623abc5893901734ff86f07310ffba5
Fi1osof <n.lanets@modxclub.ru>
2019-12-17 10:33:05 +0300

    1.7.0

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,22 @@ import ImagesMiddleware from "./middleware/ImageThumb";
 
 import PrismaUploadModule from "./modules";
 
+
+import GalleryModule, {
+  GalleryProcessor,
+} from "./modules/Gallery";
+
+export {
+  GalleryModule,
+  GalleryProcessor,
+};
+
+
+export const Modules = [
+  GalleryModule,
+];
+
+
 export {
   ImagesMiddleware,
   PrismaUploadModule,
